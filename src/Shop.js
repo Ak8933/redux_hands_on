5138ca867348a0e4fc67dcbdb4268d6a93dfcd50
Akarsh8933 <akarshkatiyarak123@gmail.com>
2024-07-09 20:24:28 +0530

    updating state of selected component with the help of useDispatch hook with bindActionCreators

--- src/Shop.js
+++ src/Shop.js
@@ -2,14 +2,20 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 // import { bindActionCreators } from 'redux'
 import { actionCreators } from './state/index'
+import { bindActionCreators } from 'redux'
+
 
 const Shop = () => {
     const dispatch = useDispatch()
+    const action = bindActionCreators(actionCreators,dispatch)
   return (
     <div>
-      <button className="btn btn-primary mx-1 my-3" onClick={()=>{dispatch(actionCreators.withdrawMoney(50))}}>-</button>
+      {/* <button className="btn btn-primary mx-1 my-3" onClick={()=>{dispatch(actionCreators.withdrawMoney(50))}}>-</button>
       Perform Transaction
-      <button className="btn btn-primary mx-1 my-3" onClick={()=>{dispatch(actionCreators.depositMoney(50))}}>+</button>
+      <button className="btn btn-primary mx-1 my-3" onClick={()=>{dispatch(actionCreators.depositMoney(50))}}>+</button> */}
+      <button className="btn btn-primary mx-1 my-3" onClick={()=>{action.withdrawMoney(50)}}>-</button>
+      Perform Transaction
+      <button className="btn btn-primary mx-1 my-3" onClick={()=>{action.depositMoney(50)}}>+</button>
     </div>
   )
 }
