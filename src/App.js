b331e4e8ce52803d5a1c1e34a20235621d20e639
Akarsh8933 <akarshkatiyarak123@gmail.com>
2024-07-09 18:05:56 +0530

    added shop.js and navbar.js components

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Navbar from './Navbar';
+import Shop from './Shop';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <h1>AtoZ Store :- Each item is of Rs.50 only</h1>
+      <Shop/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Shop.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+      <button className="btn btn-primary mx-1 my-3">-</button>
+      Add this item to cart
+      <button className="btn btn-primary mx-1 my-3">+</button>
+    </div>
+  )
+}
+
+export default Shop
