60a7d1b10aec7e11f20929551e6281f613e05cb5
Akarsh8933 <akarshkatiyarak123@gmail.com>
2024-07-09 20:15:11 +0530

    updating state of selected  component with the help of useDispatch hook without bindActionCreators

--- src/Shop.js
+++ src/Shop.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+// import { bindActionCreators } from 'redux'
+import { actionCreators } from './state/index'
 
 const Shop = () => {
+    const dispatch = useDispatch()
   return (
     <div>
-      <button className="btn btn-primary mx-1 my-3">-</button>
+      <button className="btn btn-primary mx-1 my-3" onClick={()=>{dispatch(actionCreators.withdrawMoney(50))}}>-</button>
       Perform Transaction
-      <button className="btn btn-primary mx-1 my-3">+</button>
+      <button className="btn btn-primary mx-1 my-3" onClick={()=>{dispatch(actionCreators.depositMoney(50))}}>+</button>
     </div>
   )
 }
